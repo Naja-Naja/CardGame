aa384409f0441053b772ec8d809771bb0bba5b41
Naja-Naja <latimeria.h11@gmail.com>
2024-09-16 18:05:09 +0900

    first commit

---
+++ Assets/Project/Script/MainGameManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainGameManager : ModeObject
+{
+    public override void Start()
+    {
+        base.Start();
+        
+    }
+    public override void StartMode()
+    {
+
+    }
+    public override void CloseMode()
+    {
+
+    }
+    private void Update()
+    {
+        if (IsModeActive())
+        {
+
+        }
+        
+    }
+}
